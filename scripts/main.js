75d7cbb0a0db3ffe7747970f33564ec19daafc11
Katharinap1999 <katharinapauer@katharinas-mbp.wireless.hhs.nl>
2019-02-20 15:26:15 +0100

    challenge update

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,61 @@
-var myTimer = 50;
+var myTimer = 1000;
 
 var shipParameters = {
-    fuel: 70
+    fuel: 100
+
 };
 
 var fuelObject = document.querySelector('.fuelContainer');
 
 // CLOCK
 function time() {
-var date = new Date();
-    
-var hour = date.getHours();
-var min = date.getMinutes();
-var sec = date.getSeconds();
+    var date = new Date();
 
-document.getElementById("time").innerHTML = hour + " : " + min + " : " + sec;
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+
+    document.getElementById("time").innerHTML = hour + " : " + min + " : " + sec;
 
 }
 
 // CLOCK END
 
 // calling functions every second
-var count = setInterval(function() {
+var count = setInterval(function () {
     time();
     fuel();
 
 }, myTimer);
 
+var counter = 0;
 
-function fuel(){
-    shipParameters.fuel -= 1;
-    fuelObject.style.height = shipParameters.fuel + "%";
-    
-    if (shipParameters.fuel <= 50) {
-        fuelObject.classList.add('.fuelLow');
-        fuelObject.style.backgroundColor = "red";
+function fuel() {
+
+    if (counter == 0) {
+        shipParameters.fuel -= 1;
+        fuelObject.style.height = shipParameters.fuel + "%";
+        fuelObject.innerHTML = shipParameters.fuel + "%";
+
+        if (shipParameters.fuel <= 20) {
+            fuelObject.style.backgroundColor = "red";
+        }
+
+        if (shipParameters.fuel == 0) {
+            alert("You are out of Fuel!");
+            fuelObject.innerHTML = "Out of fuel";
+            counter = 1;
+        }
     }
 }
 
+function Converter(kilogramEarth) {
 
-    
+    var kilogramMars = kilogramEarth * 0.376;
 
-
-
-
-
+    document.getElementById("converter").innerHTML = kilogramEarth + " kg on earth are " + kilogramMars + " kilogram on mars";
 
 
+}
 
-
-
-
-
-
-
-
+Converter(100);
